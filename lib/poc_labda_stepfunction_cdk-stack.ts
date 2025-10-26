1fd43f5c82431c5ebeddc59f37098c804d2e9c26
Miguel <miguelmayhem92@gmail.com>
2025-10-26 11:51:14 +0100

    lambda python

--- lib/poc_labda_stepfunction_cdk-stack.ts
+++ lib/poc_labda_stepfunction_cdk-stack.ts
@@ -1,15 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 
 export class PocLabdaStepfunctionCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps){
     super(scope, id, props)
 
     const orchestrator_lambda = new lambda.Function(this, 'orchestrator', {
-      runtime: lambda.Runtime.NODEJS_LATEST,
-      handler: 'index.handler',
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler: 'main.handler',
       code: lambda.Code.fromAsset("./modules/orchestrator"),
     });
+
+    // const myStateMachine = new sfn.StateMachine(this, 'my_workflow_poc', {
+    //   definition: new tasks.LambdaInvoke(this, "launch_orchestrator", {
+    //     lambdaFunction: orchestrator_lambda
+    //   }).next(new sfn.Succeed(this, "GreetedWorld"))
+    // });
+
   }
 }
