21414fd2b01adbc6a294864c2bac7e5ffa272e44
Miguel <miguelmayhem92@gmail.com>
2025-10-26 13:29:06 +0100

    simple

--- lib/poc_labda_stepfunction_cdk-stack.ts
+++ lib/poc_labda_stepfunction_cdk-stack.ts
@@ -2,7 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as sfn from 'aws-cdk-lib/aws-stepfunctions';
-import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Action } from 'aws-cdk-lib/aws-codepipeline';
 
 export class PocLabdaStepfunctionCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps){
@@ -12,13 +13,26 @@ export class PocLabdaStepfunctionCdkStack extends cdk.Stack {
       runtime: lambda.Runtime.PYTHON_3_11,
       handler: 'main.handler',
       code: lambda.Code.fromAsset("./modules/orchestrator"),
+      functionName: 'orchestrator_function_name'
     });
 
-    // const myStateMachine = new sfn.StateMachine(this, 'my_workflow_poc', {
-    //   definition: new tasks.LambdaInvoke(this, "launch_orchestrator", {
-    //     lambdaFunction: orchestrator_lambda
-    //   }).next(new sfn.Succeed(this, "GreetedWorld"))
-    // });
+     const iam_role = new iam.Role(this, 'poc_lambda_state_gato', {
+      assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
+      roleName:'poc_lambda_statemachine_role'
+    });
+    iam_role.addToPolicy( new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ["*"],
+      actions: ['lambda:InvokeFunction']
+    }))
+    // iam_role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"))
+    // iam_role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambda_FullAccess"))
 
+
+    const myStateMachine = new sfn.StateMachine(this, 'my_workflow_poc', {
+      definitionBody: sfn.DefinitionBody.fromFile("./modules/stepfunction/test_workflow.json"),
+      stateMachineName: "my_workflow_poc_name",
+      role: iam_role
+    });
   }
 }
